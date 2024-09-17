cd4bd38e5afb58a137d6d123ebc917cff7fa000b
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2024-09-17 15:20:18 +0600

    added all operations and smooth ui

--- screipt.js
+++ screipt.js
@@ -2,6 +2,31 @@ let string = "";
 let buttons = document.querySelectorAll("button");
 for (const button of buttons) {
   button.addEventListener("click", function (event) {
-    console.log(event.target.innerText);
+    const buttonText = event.target.innerText;
+    if (buttonText === "=") {
+      try {
+        string = eval(string);
+        document.querySelector("input").value = string;
+      } catch (error) {
+        document.querySelector("input").value = "Error";
+      }
+    } else if (buttonText === "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else if (buttonText === "DEL") {
+      if (typeof string == "string") {
+        string = string.slice(0, -1);
+      } else {
+        string = string.toString();
+        string = string.slice(0, -1);
+      }
+      document.querySelector("input").value = string;
+    } else if (buttonText === "%") {
+      string = eval(string) / 100;
+      document.querySelector("input").value = string;
+    } else {
+      string += buttonText;
+      document.querySelector("input").value = string;
+    }
   });
 }
