04fdd30cc1e5517d5f247375fcc218065cc139c0
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2024-09-17 12:55:43 +0600

    initial js file

--- screipt.js
+++ screipt.js
@@ -1,1 +1,7 @@
-console.log("co");
+let string = "";
+let buttons = document.querySelectorAll("button");
+for (const button of buttons) {
+  button.addEventListener("click", function (event) {
+    console.log(event.target.innerText);
+  });
+}
